c2e0b21c097a206164854706c8c2f07a02f902ea
Jason Brown <loktar69@hotmail.com>
2014-09-16 23:03:44 -0500

    Refactored tetris rendering now multiple instances can be running

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,11 +2,19 @@
 var tetris          = require('./tetris');
 
 function ready(){
-    document.removeEventListener( "DOMContentLoaded", ready, false );
-    window.removeEventListener( "load", ready, false );
+    document.removeEventListener( 'DOMContentLoaded', ready, false );
+    window.removeEventListener( 'load', ready, false );
 
-    tetris();
+    var width = window.innerWidth,
+        boards = ~~((window.innerWidth/20)/10),
+        bWidth = 20 * Math.round((width/10)/20),
+        tetrisInstances = [];
+
+    for(var w = 0; w < boards; w++){
+        console.log(20 * Math.round((w*bWidth)/20));
+        tetrisInstances.push(new tetris(20 * Math.round((w*bWidth)/20), 0, bWidth));
+    }
 }
 
-document.addEventListener( "DOMContentLoaded", ready, false );
-window.addEventListener( "load", ready, false );
+document.addEventListener( 'DOMContentLoaded', ready, false );
+window.addEventListener( 'load', ready, false );
